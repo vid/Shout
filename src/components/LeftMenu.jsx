0bf9d87075cff8b460b6210383610bff9dbc1cbd
Rafiya <rafiyajaved@gmail.com>
2016-11-01 16:17:16 -0700

    added icons to MenuItems

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -2,11 +2,22 @@ import React, {Component} from 'react';
 import FontIcon from 'material-ui/FontIcon';
 import Paper from 'material-ui/Paper';
 import MenuItem from 'material-ui/MenuItem';
+import Menu from 'material-ui/Menu';
+
+
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import SocialShare from 'material-ui/svg-icons/social/share';
+import ActionSearch from 'material-ui/svg-icons/action/search';
+import MapsEditLocation from 'material-ui/svg-icons/maps/edit-location';
+import SocialPerson from 'material-ui/svg-icons/social/person';
+
+
+import Divider from 'material-ui/Divider';
 
 const style = {
  position: 'fixed',
   padding:'70px 5px 15px',
-  width: '20%',
+  width: '40%',
   height: '100%',
 };
 
@@ -20,12 +31,12 @@ export default class LeftMenu extends React.Component {
         <div id='menu'>
            <Paper zDepth={1} style={style}>
            
-            <MenuItem primaryText="New Search" />
-            <MenuItem primaryText="Add New Resource" />
-            <MenuItem primaryText="Change Location" />
-
-            <MenuItem primaryText="About" />
-            <MenuItem primaryText="Invite friends" />
+            <MenuItem primaryText="New Search" leftIcon={<ActionSearch />}/>
+            <MenuItem primaryText="Add New Resource" leftIcon={<ContentAdd />}/>
+            <MenuItem primaryText="Change Location" leftIcon={<MapsEditLocation />}/>
+                <Divider />
+            <MenuItem primaryText="About" leftIcon={<SocialPerson />}/>
+            <MenuItem primaryText="Invite friends" leftIcon={<SocialShare />}/>
           
           </Paper>
             </div>
