7da27c3b90de5ba3a72c8d201ed4aa62a0d9ad45
Rafiya <rafiyajaved@gmail.com>
2016-11-12 19:38:01 -0500

    updated resources.js list

--- src/lib/resources.js
+++ src/lib/resources.js
@@ -2,21 +2,102 @@
 'use strict';
 
 export const resources = [
-  {name: "Open Door Community", civic_address: "910 Ponce De Leon Ave NE", phone: "(404) 874-9652", description: "description info description info description info"},
-  {name: "Community Advance Practice Nurses", civic_address: "173 Boulevard NE", z: "Atlanta", postal_code: "30312", phone: "(404) 658-1500"},
-  {name: "Mercy Care at The Gateway", civic_address: "424 Decatur St SE Atlanta", postal_code: "30312", phone: "(678) 843-8600"},
-  {name: "Metro Atlanta Task Force for the Homeless", civic_address: "477 Peachtree St NE Atlanta", postal_code: "30308", phone: "(404) 787-5826"},
-  {name: "Atlanta Day Shelter For Women & Children"},
-  {name: "Good Samaritan Health Center"},
-  {name: "Atlanta Childrens Shelter", phone: "(678) 843-8700"},
-  {name: "Mercy Care North", civic_address: "Northeast Plaza, 3367 Buford Hwy NE #910", postal_code: "30319 "},
-  {name: "Georgia Law Center for the Homeless", civic_address: "One Park Tower, 34 Peachtree St NW #750", postal_code: "30303", phone: "(404) 681-0680"},
-  {name: "Center for Black Women’s Wellness"},
-  {name: "Grant Park Clinic"},
-  {name: "Mercy Care"},
-  {name: "National AIDS Education and Services for Minorities Inc."},
-  {name: "Richmond County Medical Society Project Access"},
-  {name: "Urban Health and Wellness"},
-  {name: "The Center for Pan Asian Community Services, Inc."},
-  {name: "Richmond County Medical Society Project Access", civic_address: "2612 Commons Boulevard", z: "Augusta", postal_code: "30909", phone: "706-733-5177"}
+  {
+      name: "Open Door Community",
+      civic_address: "910 Ponce De Leon Ave NE", 
+      phone: "(404) 874-9652", 
+      description: "The Open Door Community is a residential community in the Catholic Worker tradition (we’re sometimes called a Protestant Catholic Worker House). We seek to dismantle racism, sexism and heterosexism, abolish the death penalty, and proclaim the Beloved Community through loving relationships with some of the most neglected and outcast of God’s children: the homeless and our sisters and brothers who are in prison. We serve breakfasts and soup-kitchen lunches, provide showers and changes of clothes, staff a free medical clinic, conduct worship services and meetings for the clarification of thought, and provide a prison ministry, including monthly trips for families to visit loved ones at the Hardwick Prisons in central Georgia. We also advocate on behalf of the oppressed, homeless and prisoners through non-violent protests, grassroots organizing and the publication of our monthly newspaper, Hospitality."
+  },
+  {
+      name: "Community Advance Practice Nurses", 
+      civic_address: "173 Boulevard NE, Atlanta",
+      postal_code: "30312", 
+      phone: "(404) 658-1500",
+      hours: {
+      M:
+          {
+              start:"8",
+              finish:"15"
+
+          },
+      T:{
+              start:"8",
+              finish:"15"
+      },
+      W:{
+              start:"8",
+              finish:"15"
+      },
+          Th:{},
+          F:{},
+          Sat:{},
+          Sun:{}
+      },
+description: "The CAPN Clinic is a free clinic offering basic physical and mental health care and prevention education to the homeless and medically underserved in metropolitan Atlanta. It is an independent nonprofit funded by grants, businesses, and individuals.CAPN provides walk-in clinics for infants, children, teens and adults in need of medical attention for primary and acute care needs. CAPN also offers social service referrals, mental health services and healthy lifestyle counseling as well."
+},
+{
+    name: "Mercy Care at The Gateway", 
+    civic_address: "275 Pryor St SW, Atlanta", 
+    postal_code: "30303", 
+    phone: "(678) 843-8840",
+    description: "We honor the healing mission of the Sisters of Mercy by providing excellent healthcare to the poor and marginalized.Our fundamentals are compassion, commitment to the poor, excellence, integrity, justice, stewardship and reverence for the dignity of each person. We are rich with workers, volunteers, board members, collaborators, funders; excellent facilities and vehicle fleets; and professional and technological expertise."
+},
+    
+{
+name: "Metro Atlanta Task Force for the Homeless", 
+    civic_address: "477 Peachtree St NE, Atlanta", 
+postal_code: "30308", 
+phone: "(404) 787-5826"
+},
+{
+    name: "Atlanta Day Shelter For Women & Children",
+    civic_address:"655 Ethel St NW, Atlanta",
+    postal_code: "30318"
+},
+  
+{
+name: "Good Samaritan Health Center", 
+},
+{
+name: "Atlanta Childrens Shelter", 
+phone: "(678) 843-8700"
+},
+{
+    name: "Mercy Care North", 
+    civic_address: "Northeast Plaza, 3367 Buford Hwy NE #910", postal_code: "30319 "
+},
+{name: 
+    "Georgia Law Center for the Homeless", 
+    civic_address: "One Park Tower, 34 Peachtree St NW #750", 
+postal_code: "30303", 
+phone: "(404) 681-0680"
+},
+{
+    name: "Center for Black Women’s Wellness"
+},
+{
+name: "Grant Park Clinic"
+},
+{
+    name: "Mercy Care"
+},
+{
+name: "National AIDS Education and Services for Minorities Inc."
+},
+{
+    name: "Richmond County Medical Society Project Access"
+},
+{
+name: "Urban Health and Wellness"
+},
+{
+    name: "The Center for Pan Asian Community Services, Inc."
+},
+{
+name: "Richmond County Medical Society Project Access", 
+    civic_address: "2612 Commons Boulevard", 
+z: "Augusta", 
+postal_code: "30909", 
+phone: "706-733-5177"
+}
 ];
